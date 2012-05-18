0e00b03a0154113d51e193713f1b75f2df357ba3
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2012-05-18 11:11:49 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -4,18 +4,21 @@ Flask-WhooshAlchemy
 
 Whoosh extension to Flask/SQLAlchemy
 """
+
 from setuptools import setup
+import os
 
 
 setup(
     name='Flask-WhooshAlchemy',
-    version='0.3a',
+    version='0.4a',
     url='https://github.com/gyllstromk/Flask-WhooshAlchemy',
     license='BSD',
     author='Karl Gyllstrom',
     author_email='karl.gyllstrom+code@gmail.com',
     description='Whoosh extension to Flask/SQLAlchemy',
-    long_description=__doc__,
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+
     py_modules=['flask_whooshalchemy'],
     # if you would be using a package instead use packages instead
     # of py_modules:
@@ -23,9 +26,10 @@ setup(
     zip_safe=False,
     include_package_data=True,
     platforms='any',
-    install_requires=[
-        'Flask', 'Flask-SQLAlchemy', 'whoosh', 'blinker'
-    ],
+    install_requires=[x.strip() for x in
+        open(os.path.join(os.path.dirname(__file__),
+            'requirements.txt')).xreadlines()],
+
     classifiers=[
         #'Environment :: Web Environment',
         #'Intended Audience :: Developers',
@@ -36,5 +40,4 @@ setup(
         #'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     test_suite = 'test.test_all',
-
 )
